d55b2c0e8d166e6fbce345eb5ccef766c0c538fd
fernandocortez <development+github@fernandocortez.dev>
2023-07-10 22:58:44 -0400

    feat: Fix full height layout; added options section to customize alignment of controls

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,20 @@
   const polyhedrals: ReadonlyArray<number> = [4, 6, 8, 10, 12, 20, 100];
   let dice: Die[] = [];
   $: results = dice.map(die => die.result);
+  
+  let viewOptions: boolean = false;
+  let options: Options = {
+    alignAddDiceButtons: 'right',
+    reverseAddDiceButtons: false,
+    alignDiceControls: 'right',
+    reverseDiceControls: true,
+  };
+  type Options = {
+    alignAddDiceButtons: 'left' | 'right',
+    reverseAddDiceButtons: boolean,
+    alignDiceControls: 'left' | 'right',
+    reverseDiceControls: boolean,
+  };
 
   function addDie(sides: number) {
     const result = getDieRoll(sides);
@@ -39,51 +53,106 @@
   function clearDice() {
     dice = [];
   }
+
+  function toggleViewOptions() {
+    viewOptions = !viewOptions;
+  }
 </script>
 
-<article class="results">
-  Total: {results.reduce((result, acc) => result + acc, 0)}
-  &nbsp;|&nbsp;
-  Max:&nbsp;
-  {#if results.length === 0}
-  0
-  {:else}
-  {Math.max(...results)}
-  {/if}
-  &nbsp;|&nbsp;
-  Min:&nbsp;
-  {#if results.length === 0}
-  0
-  {:else}
-  {Math.min(...results)}
-  {/if}
-</article>
+{#if viewOptions}
+<form class="options">
+  <p>
+    Align dice buttons:
+    <label><input type="radio" bind:group={options.alignAddDiceButtons} name="align-dice-buttons" value="left" />&nbsp;left</label>
+    <label><input type="radio" bind:group={options.alignAddDiceButtons} name="align-dice-buttons" value="right" />&nbsp;right</label>
+  </p>
+  <label>Reverse dice buttons: <input type=checkbox bind:checked={options.reverseAddDiceButtons} /></label>
 
-<article class="dice">
-  {#each dice as die, i}
-  <section class="die">
-    <button on:click={() => rollDie(i)}>
-      {die.result} (d{die.sides})
-    </button>
-    <button class="remove" on:click={() => removeDie(i)}>&times;</button>
+  <p>
+    Align control buttons:
+    <label><input type="radio" bind:group={options.alignDiceControls} name="align-dice-controls" value="left" />&nbsp;left</label>
+    <label><input type="radio" bind:group={options.alignDiceControls} name="align-dice-controls" value="right" />&nbsp;right</label>
+  </p>
+  <label>Reverse dice buttons: <input type=checkbox bind:checked={options.reverseDiceControls} /></label>
+
+  <button on:click={toggleViewOptions}>Save</button>
+</form>
+{:else}
+<article class="app">
+  <section class="results">
+    <div>
+    Total: {results.reduce((result, acc) => result + acc, 0)}
+    </div>
+    <div>|</div>
+    <div>Max:&nbsp;
+    {#if results.length === 0}
+    0
+    {:else}
+    {Math.max(...results)}
+    {/if}
+    </div>
+    <div>|</div>
+    <div>Min:&nbsp;
+    {#if results.length === 0}
+    0
+    {:else}
+    {Math.min(...results)}
+    {/if}
+    </div>
   </section>
-  {/each}
-</article>
 
-<article class="add-dice">
-  {#each polyhedrals as sides}
-  <button on:click={() => addDie(sides)}>
-    +d{sides}
-  </button>
-  {/each}
-</article>
+  <section class="dice">
+    {#each dice as die, i}
+    <div class="die">
+      <button on:click={() => rollDie(i)}>
+        {die.result} (d{die.sides})
+      </button>
+      <button class="remove" on:click={() => removeDie(i)}>&times;</button>
+    </div>
+    {/each}
+  </section>
 
-<article class="controls">
-  <button on:click={rollAllDice}>Roll All</button>
-  <button on:click={clearDice}>Clear</button>
+  <section
+    class="add-dice"
+    class:flex-align-right={!options.reverseAddDiceButtons && options.alignAddDiceButtons === 'right' || options.reverseAddDiceButtons && options.alignAddDiceButtons === 'left'}
+    class:flex-reverse={options.reverseAddDiceButtons}
+  >
+    {#each polyhedrals as sides}
+    <button on:click={() => addDie(sides)}>
+      +d{sides}
+    </button>
+    {/each}
+  </section>
+
+  <section
+    class="controls"
+    class:flex-align-right={!options.reverseDiceControls && options.alignDiceControls === 'right' || options.reverseDiceControls && options.alignDiceControls === 'left'}
+    class:flex-reverse={options.reverseDiceControls}
+  >
+    <button on:click={rollAllDice}>Roll All</button>
+    <button on:click={clearDice}>Clear</button>
+    <button on:click={toggleViewOptions} class="options">&#x2699;</button>
+  </section>
 </article>
+{/if}
 
 <style>
+  form.options {
+    padding: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    row-gap: 12pt;
+  }
+
+  .app {
+    height: 100vh;
+    min-height: 100vh;
+    padding: 0.5rem;
+    display: grid;
+    grid-template-rows: auto 1fr auto auto;
+    row-gap: 1rem;
+  }
+
   button {
     min-width: 44pt;
     min-height: 44pt;
@@ -93,26 +162,38 @@
   }
 
   .results {
-    text-align: center;
+    display: flex;
+    column-gap: 0.5em;
+    justify-content: center;
   }
 
   .dice {
     display: flex;
     flex-wrap: wrap;
-    justify-content: center;
     gap: 4pt;
+  }
+
+  .flex-align-right {
+    justify-content: flex-end;
+  }
+  .flex-reverse {
+    flex-direction: row-reverse;
   }
 
   .add-dice {
     display: flex;
     flex-wrap: wrap;
-    justify-content: flex-end;
+    /* justify-content: flex-end; */
     gap: 4pt;
   }
 
   .controls {
     display: flex;
-    flex-direction: row-reverse;
+    /* flex-direction: row-reverse; */
     gap: 4pt;
+  }
+
+  .controls .options {
+    font-size: 28pt;
   }
 </style>
